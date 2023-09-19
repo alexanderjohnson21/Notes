fcd348f8214d09de0aeaa1a38edb179ee1de1405
Alexander Johnson (Class of 2024) <240850aj@internal.salesianum.org>
2023-09-19 11:54:30 -0400

    chapter 2 notes

---
+++ Chp2.java
@@ -0,0 +1,30 @@
+package Notes;
+
+public class Chp2 {
+    
+    public static void main(String[] args){
+        
+        // classes are made up of atributes and behaviors
+
+        // if dog is the class 
+        //Atributes of dogs=
+        // fur; tail; ears; paws; breed; nose; size
+        // Behaviors of dogs=
+        // run; walk; sit; lay down; shake; eat; drink; smell/sniff; bark; play; sleep; 
+
+
+    
+
+
+
+    }
+
+
+
+
+
+
+
+}
+
+
